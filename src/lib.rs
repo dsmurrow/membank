cf0d73b6037142c66b26a477dcbd0e8fcceb7f90
Dan Windows <dsmurrow@gmail.com>
2023-10-18 21:39:54 -0400

    fluffed up the manifest, changed allocated indeces in MemoryBank from BinaryHeap to Vec

--- src/lib.rs
+++ src/lib.rs
@@ -4,23 +4,25 @@
 extern crate alloc;
 
 #[cfg(not(feature = "std"))]
-use alloc::{vec, vec::Vec};
+use alloc::vec::Vec;
+
+#[cfg(feature = "std")]
+use std::rc::{Rc, Weak};
+#[cfg(not(feature = "std"))]
+use alloc::rc::{Rc, Weak};
 
 #[cfg(not(feature = "std"))]
 use core as std;
 
 use std::cell::RefCell;
-use std::cmp::Reverse;
-use std::collections::BinaryHeap;
 use std::ops::{Deref, DerefMut, Drop};
-use std::rc::Rc;
 
 /// A smart pointer for memory borrowed from a `MemoryBank<T>`. The data allocation will be preserved
 /// even if the initiating `MemoryBank` is dropped.
 pub struct Loan<T> {
     reference: Rc<T>,
     list_index: usize,
-    parent_index_heap: Rc<RefCell<BinaryHeap<Reverse<usize>>>>,
+    parent_index_list: Weak<RefCell<Vec<usize>>>,
 }
 
 impl<T> Deref for Loan<T> {
@@ -39,16 +41,16 @@ impl<T> DerefMut for Loan<T> {
 
 impl<T> Drop for Loan<T> {
     fn drop(&mut self) {
-        self.parent_index_heap
-            .borrow_mut()
-            .push(Reverse(self.list_index));
+        if let Some(index_list) = self.parent_index_list.upgrade() {
+            index_list.borrow_mut().push(self.list_index);
+        }
     }
 }
 
 /// A structure that reuses old data of type `T` to reduce the number of heap allocations.
 pub struct MemoryBank<T> {
     list: Vec<Rc<T>>,
-    available_indeces: Rc<RefCell<BinaryHeap<Reverse<usize>>>>,
+    available_indeces: Rc<RefCell<Vec<usize>>>,
 }
 
 impl<T: Default> Default for MemoryBank<T> {
@@ -62,11 +64,14 @@ impl<T: Default> MemoryBank<T> {
     pub fn new() -> Self {
         Self {
             list: Vec::new(),
-            available_indeces: Rc::new(RefCell::new(BinaryHeap::new())),
+            available_indeces: Rc::new(RefCell::new(Vec::new())),
         }
     }
 
     /// Loans the user some memory to use. Will reuse an element from old memory if there are any, otherwise it will allocate on the heap.
+    ///
+    /// If the loaned memory was previously used, it will be in the exact same state it was in
+    /// right before the previous `Loan` got dropped.
     pub fn take_loan(&mut self) -> Loan<T> {
         let index = self
             .available_indeces
@@ -74,14 +79,13 @@ impl<T: Default> MemoryBank<T> {
             .pop()
             .unwrap_or_else(|| {
                 self.list.push(Rc::new(T::default()));
-                Reverse(self.list.len() - 1)
-            })
-            .0;
+                self.list.len() - 1
+            });
 
         Loan {
             reference: self.list[index].clone(),
             list_index: index,
-            parent_index_heap: self.available_indeces.clone(),
+            parent_index_list: Rc::downgrade(&self.available_indeces),
         }
     }
 }
@@ -89,6 +93,9 @@ impl<T: Default> MemoryBank<T> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[cfg(not(feature = "std"))]
+    use alloc::vec;
 
     #[test]
     fn basics() {
@@ -99,7 +106,7 @@ mod tests {
         let loan1_capacity = loan.capacity();
         drop(loan);
 
-        assert_eq!(bank.available_indeces.borrow().peek(), Some(&Reverse(0)));
+        assert_eq!(bank.available_indeces.borrow()[0], 0);
 
         let loan2 = bank.take_loan();
         assert!(bank.available_indeces.borrow().is_empty());
@@ -121,10 +128,10 @@ mod tests {
         loan2[3] = 5;
 
         drop(loan2);
-        assert_eq!(bank.available_indeces.borrow().peek(), Some(&Reverse(1)));
+        assert_eq!(*bank.available_indeces.borrow(), vec![1]);
 
         drop(loan1);
-        assert_eq!(bank.available_indeces.borrow().peek(), Some(&Reverse(0)));
+        assert_eq!(*bank.available_indeces.borrow(), vec![1, 0]);
 
         let loan3 = bank.take_loan();
         let loan4 = bank.take_loan();
